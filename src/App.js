98158ca598b744d3786d258d87cca9bacb825037
Shota <shotikoavaliani97@gmail.com>
2022-08-27 19:16:29 +0400

    Landing page visual and redirecting is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router,Route,Routes, } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
+import Personal from "./pages/Personal/Personal";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/personal" element={<Personal />} />
       </Routes>
     </Router>
   );
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    useNavigate,
+    Route,
+    Routes,
+  } from "react-router-dom";
 import "../Landing/Landing.css"
+import LOGO from "../Landing/LandingAssets/LOGO.png"
+import Group from "../Landing/LandingAssets/Group.png"
 
 function Landing() {
+    let navigate = useNavigate()
+    const onClick =()=>{
+        navigate("/personal")
+    }
   return (
     <div className='landingmain'>
-
+        <div className='logo'>
+            <img src={LOGO} alt="logo" />
+        </div>
+        <div className='group'>
+            <img src={Group} alt="group" />
+        </div>
+        <button onClick={onClick} className='button1'> ᲩᲐᲜᲐᲬᲔᲠᲘᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ </button>
+        <button className='button2'> ᲩᲐᲜᲐᲬᲔᲠᲔᲑᲘᲡ ᲡᲘᲐ </button>
     </div>
   )
 }
